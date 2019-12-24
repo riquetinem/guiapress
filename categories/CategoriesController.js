2a0a3358e313ba428f7cbfeb997f547f7bbea2b8
= <henrique714tinem@gmail.com>
2019-12-24 03:55:50 -0300

    edit categories

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -28,13 +28,13 @@ router.get("/admin/categories", (req, res) => {
             res.render("admin/categories/index", {
                 categories: categories
             });
-    });
+        });
 });
 
 router.post("/categories/delete", (req, res) => {
     let id = req.body.id;
-    if(id != undefined) {
-        if (!isNaN(id)){
+    if (id != undefined) {
+        if (!isNaN(id)) {
             Category.destroy({
                 where: {
                     id: id
@@ -48,6 +48,39 @@ router.post("/categories/delete", (req, res) => {
     } else {
         res.redirect("/admin/categories")
     }
+});
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+    let id = req.params.id;
+
+    if (isNaN(id))
+        res.redirect("/admin/categories")
+
+    Category.findByPk(id)
+        .then(category => {
+            if (category != undefined) {
+                res.render("admin/categories/edit", {
+                    category: category
+                })
+            } else {
+                res.redirect("/admin/categories")
+            }
+        }).catch(erro => {
+        res.redirect("/admin/categories")
+    })
+});
+
+router.post("/categories/update", (req, res) => {
+    let id = req.body.id;
+    let title = req.body.title;
+
+    Category.update({title: title, slug: slugify(title)}, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/categories")
+    })
 });
 
 module.exports = router;
