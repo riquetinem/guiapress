e14bbd1dbd380000b589cafb8d17f58188e1f800
= <henrique714tinem@gmail.com>
2019-12-24 03:28:26 -0300

    delete categories

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -14,7 +14,7 @@ router.post("/categories/save", (req, res) => {
             title: title,
             slug: slugify(title)
         }).then(() => {
-            res.redirect("/")
+            res.redirect("/admin/categories")
         })
     } else {
         res.redirect("/admin/categories/new")
@@ -29,6 +29,25 @@ router.get("/admin/categories", (req, res) => {
                 categories: categories
             });
     });
+});
+
+router.post("/categories/delete", (req, res) => {
+    let id = req.body.id;
+    if(id != undefined) {
+        if (!isNaN(id)){
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/categories")
+            })
+        } else {
+            res.redirect("/admin/categories")
+        }
+    } else {
+        res.redirect("/admin/categories")
+    }
 });
 
 module.exports = router;
