5a065d1a7d340a4aa9f947b116bb4ef0efae40ae
= <henrique714tinem@gmail.com>
2019-12-24 04:51:42 -0300

    init articles

--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize");
 
 const connection = new Sequelize('guiapress', 'root', 'tinem714', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: "-03:00"
 });
 
 module.exports = connection;
