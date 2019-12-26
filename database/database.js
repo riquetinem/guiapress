c815aa3c3bcd083f1a7e48fff22c7afda7dc9f03
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2019-12-26 04:08:04 -0300

    delete articles

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize('guiapress', 'root', 'tinem714', {
+const connection = new Sequelize('guiapress', 'root', '', {
     host: 'localhost',
     dialect: 'mysql',
     timezone: "-03:00"
