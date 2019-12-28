49d268e42e180b17e15a3c4a5927d7e4dee333ae
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2019-12-27 22:48:12 -0300

    User creeate and read

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const app = express();
 const bodyParser = require("body-parser");
 const connection = require("./database/database");
 
-// Routers
+// Controllers
 const categoriesController = require("./categories/CategoriesController");
 const articlesController = require("./articles/ArticlesController");
+const usersController = require("./user/UserController");
 
+// Routers
 const Article = require("./articles/Article");
 const Category = require("./categories/Category");
+const User = require("./user/User");
 
 //View engine
 app.set('view engine', 'ejs');
@@ -34,6 +37,7 @@ connection
 // import routers for app
 app.use("/", categoriesController);
 app.use("/", articlesController);
+app.use("/", usersController);
 
 app.get("/", (req, res) => {
     Article.findAll({
